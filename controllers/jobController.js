cdf68ad5502eac16e5c0061266483b15eea4bf14
Your Name <urielparavi75@gmail.com>
2025-01-29 22:38:40 +0200

    finish 71. Controller and Router

---
+++ controllers/jobController.js
@@ -0,0 +1,69 @@
+import { nanoid } from 'nanoid';
+
+let jobs = [
+  { id: nanoid(), company: 'apple', position: 'front-end' },
+  { id: nanoid(), company: 'google', position: 'back-end' },
+];
+
+// GET ALL JOBS
+export const getAllJobs = async (req, res) => {
+  res.status(200).json({ jobs });
+};
+
+// CREATE JOB
+export const createJob = async (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res.status(400).json({ msg: 'please provide company and position' });
+  }
+  const id = nanoid(10); // The number represent how big the number will be
+  const job = { id, company, position };
+  jobs.push(job);
+  res.status(201).json({ job });
+};
+
+// GET SINGLE JOB
+export const getJob = async (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+  res.status(200).json({ job });
+};
+
+// EDIT JOB
+export const updateJob = async (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res.status(400).json({ msg: 'please provide company and position' });
+  }
+
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+  // So we define our company and position in the job that we finded in our DB to be the company and the position
+  // that coming from our req.body
+  job.company = company;
+  job.position = position;
+  res.status(200).json({ msg: 'job modified', job });
+};
+
+// DELETE JOB
+export const deleteJob = async (req, res) => {
+  const { id } = req.params;
+
+  const job = jobs.find((job) => job.id === id);
+
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+
+  // So we kepping all the jobs with id that doesn't match to our id from req.params, and filtering out our job with the id
+  // that do match to our id that coming from req.params
+  const newJobs = jobs.filter((job) => job.id !== id);
+  jobs = newJobs;
+  res.status(200).json({ msg: 'job deleted' });
+};
--- server.js
+++ server.js
@@ -2,14 +2,11 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import morgan from 'morgan';
-import { nanoid } from 'nanoid';
-
-let jobs = [
-  { id: nanoid(), company: 'apple', position: 'front-end' },
-  { id: nanoid(), company: 'google', position: 'back-end' },
-];
 
 const app = express();
+
+// routers
+import jobRouter from './routes/jobRouter.js';
 
 // The process object is a global that provides information about, and control over, the current Node. js process. As a
 // global, it is always available to Node. js applications without using require() and inside of him we have
@@ -34,68 +31,7 @@ app.post('/', (req, res) => {
   res.json({ message: 'data received', data: req.body });
 });
 
-// GET ALL JOBS
-app.get('/api/v1/jobs', (req, res) => {
-  res.status(200).json({ jobs });
-});
-
-// CREATE JOB
-app.post('/api/v1/jobs', (req, res) => {
-  const { company, position } = req.body;
-  if (!company || !position) {
-    return res.status(400).json({ msg: 'please provide company and position' });
-  }
-  const id = nanoid(10); // The number represent how big the number will be
-  const job = { id, company, position };
-  jobs.push(job);
-  res.status(201).json({ job });
-});
-
-// GET SINGLE JOB
-app.get('/api/v1/jobs/:id', (req, res) => {
-  const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
-    return res.status(404).json({ msg: `no job with id ${id}` });
-  }
-  res.status(200).json({ job });
-});
-
-// EDIT JOB
-app.patch('/api/v1/jobs/:id', (req, res) => {
-  const { company, position } = req.body;
-  if (!company || !position) {
-    return res.status(400).json({ msg: 'please provide company and position' });
-  }
-
-  const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
-    return res.status(404).json({ msg: `no job with id ${id}` });
-  }
-  // So we define our company and position in the job that we finded in our DB to be the company and the position
-  // that coming from our req.body
-  job.company = company;
-  job.position = position;
-  res.status(200).json({ msg: 'job modified', job });
-});
-
-// DELETE JOB
-app.delete('/api/v1/jobs/:id', (req, res) => {
-  const { id } = req.params;
-
-  const job = jobs.find((job) => job.id === id);
-
-  if (!job) {
-    return res.status(404).json({ msg: `no job with id ${id}` });
-  }
-
-  // So we kepping all the jobs with id that doesn't match to our id from req.params, and filtering out our job with the id
-  // that do match to our id that coming from req.params
-  const newJobs = jobs.filter((job) => job.id !== id);
-  jobs = newJobs;
-  res.status(200).json({ msg: 'job deleted' });
-});
+app.use('/api/v1/jobs', jobRouter);
 
 // NOT FOUND REQUEST MIDDLEWARE
 // app.use => for all the http methods, * for all the URLs
