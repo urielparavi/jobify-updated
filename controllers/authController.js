20bf16c8400a01beaf6c29605f1203b7ee21d887
Your Name <urielparavi75@gmail.com>
2025-02-03 17:48:07 +0200

    95. Admin User finish

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,11 @@ import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
 
 export const register = async (req, res) => {
+  // So only the user with the first account will be the admin, so if a document/s /account/s already exists,
+  // there role will be user
+  const isFirstAccount = (await User.countDocuments()) === 0;
+  req.body.role = isFirstAccount ? 'admin' : 'user';
+
   const user = await User.create(req.body);
   res.status(StatusCodes.CREATED).json({ user });
 };
