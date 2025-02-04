1ee603458493728b2cca5501d2cabdbfae0a75b9
Your Name <urielparavi75@gmail.com>
2025-02-04 10:28:30 +0200

    98. Validate Login  finish

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -84,3 +84,16 @@ export const validateRegisterInput = withValidationErrors([
   body('location').notEmpty().withMessage('location is required'),
   body('lastName').notEmpty().withMessage('last name is required'),
 ]);
+
+export const validateLoginInput = withValidationErrors([
+  body('email')
+    .notEmpty()
+    .withMessage('email is required')
+    .isEmail()
+    .withMessage('invalid email format'),
+  body('password')
+    .notEmpty()
+    .withMessage('password is required')
+    .isLength({ min: 8 })
+    .withMessage('password must be at least 8 characters long'),
+]);
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 import { login, register } from '../controllers/authController.js';
-import { validateRegisterInput } from '../middleware/validationMiddleware.js';
+import {
+  validateLoginInput,
+  validateRegisterInput,
+} from '../middleware/validationMiddleware.js';
 
 const router = Router();
 
 // We don't use router.route() since we not going to chain anything
 // router.route('/register').post(register);
 router.post('/register', validateRegisterInput, register);
-router.post('/login', login);
+router.post('/login', validateLoginInput, login);
 
 export default router;
