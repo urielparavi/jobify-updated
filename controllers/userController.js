09fec330ef230357bf35b379550965c3d173cf57
Your Name <urielparavi75@gmail.com>
2025-02-06 13:03:55 +0200

    110. Get Current User finish

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,15 @@ import User from '../models/UserModel.js';
 import Job from '../models/JobModel.js';
 
 export const getCurrentUser = async (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: 'get current user' });
+  // Side note: we are removing the passwrod property from the current-user end point, even the our password is hashed,
+  // We don't wanna sent it back
+
+  // We can get the user without the Password field with select method, and this actualy a better aproach
+  // const user = await User.findOne({ _id: req.user.userId }).select('-password');
+  const user = await User.findOne({ _id: req.user.userId });
+  const userWithoutPassword = user.toJSON();
+
+  res.status(StatusCodes.OK).json({ user: userWithoutPassword });
 };
 
 export const getApplicationStats = async (req, res) => {
--- models/UserModel.js
+++ models/UserModel.js
@@ -19,4 +19,17 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
+// Instance methods - we get here the instance of the user. So we get back our user from any instance that we do in any
+// contrller, so when we get the user for example by find method in some controller, we do instace of it
+
+//  toJSON => our name that we select
+UserSchema.methods.toJSON = function () {
+  // toObject => We transform our user to JS object, so we returns an object with each property name and value corresponding
+  // obj => Our object User Model
+  let obj = this.toObject();
+  // We remove the password property, so that we cannot se it in the current-user end point
+  delete obj.password;
+  return obj;
+};
+
 export default mongoose.model('User', UserSchema);
