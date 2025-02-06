978e30eff5dba000af739c023f965ee1b7173659
Your Name <urielparavi75@gmail.com>
2025-02-06 10:35:03 +0200

    Play 109. User Routes finish

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express();
 // routers
 import jobRouter from './routes/jobRouter.js';
 import authRouter from './routes/authRouter.js';
+import userRouter from './routes/userRouter.js';
 
 // middleware
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
@@ -40,6 +41,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/v1/jobs', authenticateUser, jobRouter);
+app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
 
 // NOT FOUND REQUEST MIDDLEWARE
