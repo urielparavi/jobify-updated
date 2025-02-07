d14a4555a85933c5db1412a1dc4b23565e326c6b
Your Name <urielparavi75@gmail.com>
2025-02-07 10:56:55 +0200

    113. Proxy Setup finish

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+
+fetch('/api/v1/test')
+  .then((res) => res.json())
+  .then((data) => console.log(data));
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // So all the requests in our application in our front-end that will go to this path '/api', will directed to this
+      // URL - http://localhost:5100/api
+      '/api': {
+        target: 'http://localhost:5100/api',
+        // We change the origin which actually is going to fix the cors issue
+        changeOrigin: true,
+        // We rewrite the path and remove the api, so we're modify the path with the regular expression to remove the api prefix
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
