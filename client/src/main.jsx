fde159074862d36bf7d05897e6f0587d098e36b2
Your Name <urielparavi75@gmail.com>
2025-02-07 13:32:53 +0200

    116. React Router Action Intro finish

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import customFetch from './utils/customFetch.js';
-
-const resp = await customFetch.get('/test');
-console.log(resp);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
