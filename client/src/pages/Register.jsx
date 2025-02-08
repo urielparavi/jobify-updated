ad7dc88d3e563d24cdc7de265a49c6d4e0e24cf3
Your Name <urielparavi75@gmail.com>
2025-02-08 14:36:43 +0200

    118. Register User Complete finish

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,30 +1,44 @@
 import { Form, redirect, useNavigation, Link } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
 import { FormRow, Logo } from '../components';
+import customFetch from '../utils/customFetch';
 
 // Every time that we using this function we need to return some value from it - no matter what it is,
 // it could be null, but it need to return something
-export const action = async ({ data }) => {
-  console.log(data);
-  return null;
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  // Object.fromEntries() => turns array of arrays into an object
+  const data = Object.fromEntries(formData);
+
+  // console.log(data);// => {name:'uriel', lastName:'paravi', location:'london', email:'test@example.com', password:'test1234'}
+
+  try {
+    // .post(http://localhost:5100/api/v1/auth/register)
+    await customFetch.post('/auth/register', data);
+    return redirect('/login');
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
 };
 
 const Register = () => {
   return (
     <Wrapper>
       {/* Form => Component that coming from React router dom. it's still going to be regular form that get CSS end other elements normally, but it also able to use the action function */}
+      {/* If we wanna check the validation form, we can do it with noValidate => this disabled the required from the browser */}
       <Form method="post" className="form">
         <Logo />
         <h4>Register</h4>
-        <FormRow type="text" name="name" defaultValue="john" />
+        <FormRow type="text" name="name" defaultValue="uriel" />
         <FormRow
           type="text"
           name="lastName"
           labelText="last name"
-          defaultValue="smith"
+          defaultValue="paravi"
         />
         <FormRow type="text" name="location" defaultValue="london" />
-        <FormRow type="email" name="email" defaultValue="john@example.com" />
+        <FormRow type="email" name="email" defaultValue="test@example.com" />
         <FormRow type="password" name="password" defaultValue="test1234" />
         <button type="submit" className="btn btn-block">
           submit
