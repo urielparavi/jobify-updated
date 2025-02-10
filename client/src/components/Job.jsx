1ba8cb5be9e9ad60868ae6e4584e9005990fb4e0
Your Name <urielparavi75@gmail.com>
2025-02-10 13:11:30 +0200

    134. Job Component finish

--- client/src/components/Job.jsx
+++ client/src/components/Job.jsx
@@ -1,4 +1,51 @@
-const Job = ({ ...job }) => {
-  return <div>{job.position}</div>;
+import { FaLocationArrow, FaBriefcase, FaCalendarAlt } from 'react-icons/fa';
+import { Link, Form } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/Job';
+import JobInfo from './JobInfo';
+import day from 'dayjs';
+import advancedFormat from 'dayjs/plugin/advancedFormat';
+day.extend(advancedFormat);
+
+const Job = ({
+  _id,
+  position,
+  company,
+  jobLocation,
+  jobType,
+  createdAt,
+  jobStatus,
+}) => {
+  // console.log(createdAt); // => 2025-02-09T18:46:33.562Z
+  // Because our date not looking good, we format it with a shape of a month, day and year
+  const date = day(createdAt).format('MMM Do, YYYY');
+
+  return (
+    <Wrapper>
+      <header>
+        {/* charAt(0) => We take the first character from the company property. So google will be G  etc..*/}
+        <div className="main-icon">{company.charAt(0)}</div>
+        <div className="info">
+          <h5>{position}</h5>
+          <p>{company}</p>
+        </div>
+      </header>
+      <div className="content">
+        <div className="content-center">
+          <JobInfo icon={<FaLocationArrow />} text={jobLocation} />
+          <JobInfo icon={<FaCalendarAlt />} text={date} />
+          <JobInfo icon={<FaBriefcase />} text={jobType} />
+          <div className={`status ${jobStatus}`}>{jobStatus}</div>
+        </div>
+        <footer className="actions">
+          <Link className="btn edit-btn">Edit</Link>
+          <Form>
+            <button type="submit" className="btn delete-btn">
+              Delete
+            </button>
+          </Form>
+        </footer>
+      </div>
+    </Wrapper>
+  );
 };
 export default Job;
