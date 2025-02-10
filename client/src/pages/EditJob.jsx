4d1fff2d0308c77dae95e2c0eecbcaa75bbc43e5
Your Name <urielparavi75@gmail.com>
2025-02-10 20:50:27 +0200

    139. Edit Job JSX finish

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -28,8 +28,46 @@ const EditJob = () => {
   // const params = useParams();
   // console.log(params);
   const { job } = useLoaderData();
-  console.log(job);
+  // console.log(job);
+  const navigation = useNavigation();
 
-  return <h1>EditJob Page</h1>;
+  const isSubmitting = navigation.state === 'submitting';
+
+  return (
+    <Wrapper>
+      <Form method="post">
+        <h4 className="form-title">edit job</h4>
+        <div className="form-center">
+          <FormRow type="text" name="position" defaultValue={job.position} />
+          <FormRow type="text" name="company" defaultValue={job.company} />
+          <FormRow
+            type="text"
+            name="jobLocation"
+            labelText="job location"
+            defaultValue={job.jobLocation}
+          />
+          <FormRowSelect
+            name="jobStatus"
+            labelText="job status"
+            defaultValue={job.jobStatus}
+            list={Object.values(JOB_STATUS)}
+          />
+          <FormRowSelect
+            name="jobType"
+            labelText="job type"
+            defaultValue={job.jobType}
+            list={Object.values(JOB_TYPE)}
+          />
+          <button
+            type="submit"
+            className="btn btn-block form-btn"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? 'submitting...' : 'submit'}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
+  );
 };
 export default EditJob;
