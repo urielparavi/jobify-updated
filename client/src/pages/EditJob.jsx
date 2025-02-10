a765b76cce01b48da0b39788cacc8151305a20f0
Your Name <urielparavi75@gmail.com>
2025-02-10 19:55:20 +0200

    138. Edit Job Loader finish

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -9,9 +9,14 @@ import customFetch from '../utils/customFetch';
 // if we want to access to the params, route params/path variable/path parameter in the actual component,
 //  we can destructure it from our functions - our loader and the action functions
 export const loader = async ({ params }) => {
-  console.log(params);
-
-  return null;
+  // console.log(params); // => { id: "67a8f8095f669ff40c9e1a54" }
+  try {
+    const { data } = await customFetch.get(`/jobs/${params.id}`);
+    return data;
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return redirect('/dashboard/all-jobs');
+  }
 };
 
 export const action = async () => {
@@ -22,6 +27,8 @@ const EditJob = () => {
   // The way for access our params in the actual component if we don't have action/loader functions
   // const params = useParams();
   // console.log(params);
+  const { job } = useLoaderData();
+  console.log(job);
 
   return <h1>EditJob Page</h1>;
 };
