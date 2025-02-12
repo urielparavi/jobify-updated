5dcf349fff0cd6f8cafa5d9f09b578894596b301
Your Name <urielparavi75@gmail.com>
2025-02-12 14:17:07 +0200

    148. Profile Page Structure finish

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,4 +1,55 @@
+import { FormRow } from '../components';
+import Wrapper from '../assets/wrappers/DashboardFormPage';
+import { useOutletContext } from 'react-router-dom';
+import { useNavigation, Form } from 'react-router-dom';
+import customFetch from '../utils/customFetch';
+import { toast } from 'react-toastify';
+
 const Profile = () => {
-  return <h1>Profile Page</h1>;
+  const { user } = useOutletContext();
+  const { name, lastName, email, location } = user;
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+
+  return (
+    <Wrapper>
+      <Form method="post" className="form" encType="multipart/form-data">
+        <h4 className="form-title">profile</h4>
+        <div className="form-center">
+          <div className="form-row">
+            <label htmlFor="avatar" className="form-label">
+              Select an image file (max 0.5 MB)
+            </label>
+            <input
+              type="file"
+              id="avatar"
+              name="avatar"
+              className="form-input"
+              // The accept attribute takes as its value a comma-separated list of one or more file types, or unique file type
+              //  specifiers, describing which file types to allow.
+              // So we looking only specific type of files, and it going to be image files
+              accept="image/*"
+            />
+          </div>
+          <FormRow type="text" name="name" defaultValue={name} />
+          <FormRow
+            type="text"
+            name="lastName"
+            labelText="last name"
+            defaultValue={lastName}
+          />
+          <FormRow type="email" name="email" defaultValue={email} />
+          <FormRow type="text" name="location" defaultValue={location} />
+          <button
+            className="btn btn-block form-btn"
+            type="submit"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? 'submitting...' : 'submit'}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
+  );
 };
 export default Profile;
